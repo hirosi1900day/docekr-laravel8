7af643cffd748edc4cfaeba2829f7d34295afce7
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2021-06-21 19:11:58 +0900

    vue-routerのセットアップが完了しました

---
+++ backend/resources/views/register.blade.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>registerです</title>
+
+  <!-- Scripts -->
+ 
+
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Merriweather|Roboto:400">
+  <link rel="stylesheet" href="https://unpkg.com/ionicons@4.2.2/dist/css/ionicons.min.css">
+
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ mix('js/app.js') }}">
+</head>
+<body>
+  <h1>regiterです</h1>
+  
+</body>
+<script src="/js/app.js"></script>
+</html>
